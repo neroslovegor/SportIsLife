2a5329f2938a029acbea1e1c9252250028e5a748
Egor Neroslov <egor.neroslov@mail.ru>
2022-12-08 20:52:58 +0500

    completed weight tracking tab

--- app/src/main/java/com/example/sportislife/ui/weight/WeightTrackingFragment.java
+++ app/src/main/java/com/example/sportislife/ui/weight/WeightTrackingFragment.java
@@ -1,82 +1,98 @@
 package com.example.sportislife.ui.weight;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Application;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.sportislife.R;
-import com.example.sportislife.databinding.FragmentBodyBinding;
+import com.example.sportislife.adapter.WeightTrackingAdapter;
+import com.example.sportislife.dao.DaoWeight;
 import com.example.sportislife.databinding.FragmentWeightTrackingBinding;
-import com.example.sportislife.databinding.FragmentWorkoutBinding;
-import com.example.sportislife.ui.body.BodyViewModel;
+import com.example.sportislife.db.AppDatabase;
+import com.example.sportislife.repository.WeightRepository;
 import com.github.mikephil.charting.charts.LineChart;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class WeightTrackingFragment extends Fragment implements View.OnClickListener {
+public class WeightTrackingFragment extends Fragment {
 
     private WeightTrackingViewModel viewModel;
     private FragmentWeightTrackingBinding binding;
 
     private EditText editTextDate, editTextWeight;
-    private FloatingActionButton btnSaveWeight;
+    //private FloatingActionButton btnSaveWeight;
     private LineChart weightLineChart;
-
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(this, new WeightTrackingFactory(getContext())).get(WeightTrackingViewModel.class);
-        binding = FragmentWeightTrackingBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        editTextDate = binding.editTextDate;
-        viewModel.getCurrentDate().observe(getViewLifecycleOwner(), editTextDate::setText);
-
-        editTextWeight = binding.editTextWeight;
-
-        btnSaveWeight = binding.btnSaveWeight;
-        btnSaveWeight.setOnClickListener(this);
-
-        weightLineChart = binding.weightLineChart;
-        viewModel.getWeightData().observe(getViewLifecycleOwner(), weightLineChart::setData);
-
-        return root;
-    }
+    private ListView listViewHistoryWeight;
+    private WeightTrackingAdapter weightTrackingAdapter;
 
     @Override
-    public void onClick(View view) {
-        if (view == btnSaveWeight) {
-            save();
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Application application = this.requireActivity().getApplication();
+        DaoWeight dao = AppDatabase.getInstance(application).daoWeight();
+        WeightRepository repository = new WeightRepository(dao);
+        WeightTrackingFactory factory = new WeightTrackingFactory(repository, application   );
 
-    private void save() {
-        try {
-            String textDate = editTextDate.getText().toString();
-            String textWeight  = editTextWeight.getText().toString();
+        viewModel = new ViewModelProvider(this, factory).get(WeightTrackingViewModel.class);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_weight_tracking, container, false);
+        binding.setMyViewModel(viewModel);
+        binding.setLifecycleOwner(this);
 
-            if (!TextUtils.isEmpty(textDate) && !TextUtils.isEmpty(textWeight)) {
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-                Date date = simpleDateFormat.parse(textDate);
-                Float weight = Float.parseFloat(textWeight);
-                viewModel.saveWeightData(date, weight);
-            } else {
+        editTextDate = binding.editTextDate;
+        editTextWeight = binding.editTextWeight;
+        weightLineChart = binding.weightLineChart;
+        listViewHistoryWeight = binding.listViewHistoryWeight;
+
+        viewModel.getCurrentDate().observe(getViewLifecycleOwner(), editTextDate::setText);
+
+        //viewModel.getWeightData().observe(getViewLifecycleOwner(), weightLineChart::setData);
+        viewModel.getLineChart().observe(getViewLifecycleOwner(), lineChart -> {
+            weightLineChart.getDescription().setText("Weight/Date");
+            weightLineChart.setData(lineChart.getData());
+            weightLineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+            weightLineChart.getXAxis().setValueFormatter(lineChart.getXAxis().getValueFormatter());
+            weightLineChart.getXAxis().setGranularity(1f);
+        });
+
+        viewModel.getWeightData().observe(getViewLifecycleOwner(), weightData -> {
+            weightTrackingAdapter = new WeightTrackingAdapter(application.getApplicationContext(), weightData);
+            listViewHistoryWeight.setAdapter(weightTrackingAdapter);
+        });
+        listViewHistoryWeight.setOnItemClickListener((parent, view, position, id) -> {
+            final int itemId = (int) weightTrackingAdapter.getItemId(position);
+            viewModel.deleteButton(itemId);
+        });
+
+        viewModel.getErrorWeight().observe(getViewLifecycleOwner(), hasError -> {
+            if (hasError) {
                 editTextWeight.setError(getString(R.string.error_this_field_cannot_be_empty));
+                viewModel.doneErrorWeight();
             }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        });
+
+//        editTextDate = binding.editTextDate;
+//        viewModel.getCurrentDate().observe(getViewLifecycleOwner(), editTextDate::setText);
+//
+//        editTextWeight = binding.editTextWeight;
+//
+//        btnSaveWeight = binding.btnSaveWeight;
+//        btnSaveWeight.setOnClickListener(this);
+//
+//        weightLineChart = binding.weightLineChart;
+//        viewModel.getWeightData().observe(getViewLifecycleOwner(), weightLineChart::setData);
+
+        return binding.getRoot();
     }
 
     @Override
